586db2c1a07fa11540957cd769ef86a16d444642
usman ahmed khan <usmanahmedkhan@live.com>
2023-05-12 16:26:36 +0500

    review card is added

--- src/components/dashboard-components/customer-reviews.vue
+++ src/components/dashboard-components/customer-reviews.vue
@@ -4,13 +4,36 @@
             <h4>Customer Reviews</h4>
             <p>What our Customers say about Priceoye.pk</p>
         </div>
-        <div class="customer__reviews--card">
-
+        <div class="customer__reviews--content">
+            <div class="customer__reviews--content--card"
+                 v-for="(item, index) in reviews"
+                 :key="index">
+                <div class="user__profile">
+                    <div class="image__wrapper">
+                        <img :src="getImageUrl('user-icon-white.svg', 'svg')"
+                             alt="">
+                    </div>
+                    <div class="user__description">
+                        <p class="user__description--name">{{ item.name }} <span>Verified Customer</span></p>
+                        <div class="user__description--rating">
+                            <img v-for="item in [1, 2, 3, 4, 5]"
+                                 :key="item"
+                                 :src="getImageUrl('stars.svg', 'svg')"
+                                 alt="">
+                        </div>
+                        <p class="user__description--date">11 May 2023</p>
+                    </div>
+                </div>
+                <div class="user__review">
+                    <p>{{ item.review }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import utilService from '@/services/util.service'
 
 export default defineComponent({
     setup()
@@ -25,7 +48,8 @@ export default defineComponent({
             { review: 'The Best Marketplace in the Country... Iam speechless to be honest... LOWEST PRICE GENUINE PRODUCT FREE DELIVERY and the Best thing was that PACKAGING VIDEO', name: 'Atthar' }
         ])
         return {
-            reviews
+            reviews,
+            getImageUrl: utilService.getImageUrl
         }
     },
 })
@@ -42,6 +66,72 @@ export default defineComponent({
 
         h4 {
             font-weight: 500;
+        }
+    }
+
+    &--content {
+        display: flex;
+        overflow-x: auto;
+        margin-top: 32px;
+
+        &::-webkit-scrollbar {
+            width: 0px;
+        }
+
+        &--card {
+            background: #48afff;
+            color: #fff;
+            border-radius: 8px;
+            padding: 20px;
+            height: 270px;
+            min-width: 389px;
+            margin-left: 14px;
+            position: relative;
+            overflow: hidden;
+
+            .user__profile {
+                display: flex;
+
+                .user__description {
+                    margin-left: 16px;
+
+                    &--name {
+                        font-size: 1.4rem;
+                        color: white;
+
+                        span {
+                            color: #fff600;
+                            margin-left: 5px;
+
+                        }
+                    }
+
+                    &--rating {
+                        display: flex;
+                        gap: 5px;
+                    }
+
+                    &--date {
+                        font-size: 1.4rem;
+                        color: white;
+                        margin-top: 3px;
+                    }
+                }
+            }
+
+            .user__review {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 3;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                margin-top: 16px;
+
+                p {
+                    color: white;
+                    font-size: 1.4rem;
+                }
+            }
         }
     }
 }
