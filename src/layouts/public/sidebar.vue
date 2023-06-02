ef29b01e752a835e1fb205b7342ff4945f5f7774
usman ahmed khan <usmanahmedkhan@live.com>
2023-06-02 23:26:00 +0500

    working on sidebar

--- src/layouts/public/sidebar.vue
+++ src/layouts/public/sidebar.vue
@@ -2,11 +2,21 @@
     <div class="sidebar__overlay"
          :class="{ 'open': showSidebar }"
          @click="$emit('toggleSidebar')">
-        <div class="main__sidebar"></div>
+        <div class="main__sidebar"
+             :class="{ 'open': showSidebar }">
+            <div class="user__box">
+                <img height=""
+                     width="130"
+                     :src="getImageUrl('logo.svg', 'svg')"
+                     alt="">
+                <button class="btn btn__login">Login</button>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import utilService from '@/services/util.service';
 
 export default defineComponent({
     props: {
@@ -17,7 +27,7 @@ export default defineComponent({
     },
     setup(props)
     {
-        return {}
+        return { getImageUrl: utilService.getImageUrl }
     },
 })
 </script>
@@ -35,6 +45,31 @@ export default defineComponent({
     &.open {
         width: 100%;
         // transition: all .5s ease-in-out;
+    }
+
+    .main__sidebar {
+        width: 0;
+        height: 100%;
+        background-color: white;
+
+        &.open {
+            width: 358px;
+            transition: all .5s ease-in-out;
+        }
+
+        .user__box {
+            display: flex;
+            flex-direction: column;
+            background: var(--color-primary-blue);
+            padding: 70px 32px;
+
+            .btn__login {
+                max-width: 110px;
+                margin-top: 24px;
+                font-weight: 400;
+                padding: 5px
+            }
+        }
     }
 }
 </style>
